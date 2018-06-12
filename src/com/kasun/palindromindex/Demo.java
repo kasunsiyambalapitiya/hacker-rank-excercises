4754de0a9e9d6f9d45665d501b933b7b85ca58bc
kasunsiyambalapitiya <kasun.siyambalapitiya@gmail.com>
2018-06-12 16:41:45 +0530

    Improvements

--- src/com/kasun/palindromindex/Demo.java
+++ src/com/kasun/palindromindex/Demo.java
@@ -10,34 +10,30 @@ public class Demo {
     public static void main(String[] args) {
         Scanner user_input = new Scanner(System.in);
         int noOfQueries = user_input.nextInt();
-        String query[] = new String[noOfQueries];
-        int result[] = new int[noOfQueries];
-        for (int i = 0; i < noOfQueries; i++) {
-            query[i] = user_input.next();
-        }
-        for (int i = 0; i < query.length; i++) {
-            result[i] = palindrom(query[i]);
-        }
-        for (int i : result) {
-            System.out.println(i);
+        for (int i=0; i< noOfQueries;i++){
+            String sampleText= user_input.next();
+            System.out.println(palindrom(sampleText));
         }
     }
     // first checkt the string is equal in reverse order then -1
-
-    public static int palindrom(String text) {
-        if (new StringBuilder(text).reverse().toString().equals(text)) {
-            return -1;
-        } else {
-            // aba bnem
-            for (int i = 0; i < text.length(); i++) {
-                String tempText1 = text.substring(0, i);
-                String tempText2 = text.substring(i + 1, text.length());
-                String newText = tempText1 + tempText2;
-                if (new StringBuilder(newText).reverse().toString().equals(newText)) {
-                    return i;
+    // aaaaa
+    // aviddiva
+    public static int palindrom(String sampleText) {
+        for (int i=0; i< sampleText.length())
+        if (sampleText.charAt(0) == sampleText.charAt(sampleText.length()))
+            if (new StringBuilder(sampleText).reverse().toString().equals(sampleText)) {
+                return -1;
+            } else {
+                // aba bnem
+                for (int i = 0; i < sampleText.length(); i++) {
+                    String tempText1 = sampleText.substring(0, i);
+                    String tempText2 = sampleText.substring(i + 1, sampleText.length());
+                    String newText = tempText1 + tempText2;
+                    if (new StringBuilder(newText).reverse().toString().equals(newText)) {
+                        return i;
+                    }
                 }
             }
-        }
         return -1;
     }
 }
