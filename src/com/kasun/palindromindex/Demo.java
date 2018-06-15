00e2591e878177b27c35c842c222bd9ee5c02057
kasunsiyambalapitiya <kasun.siyambalapitiya@gmail.com>
2018-06-16 00:48:04 +0530

    Still not yet

--- src/com/kasun/palindromindex/Demo.java
+++ src/com/kasun/palindromindex/Demo.java
@@ -9,46 +9,57 @@ public class Demo {
 
     public static void main(String[] args) {
         Scanner user_input = new Scanner(System.in);
-        int noOfQueries = user_input.nextInt();
-        for (int i = 0; i < noOfQueries; i++) {
-            System.out.println(palindrom(user_input.next()));
-        }
+//        int noOfQueries = user_input.nextInt();
+//        for (int i = 0; i < noOfQueries; i++) {
+//            System.out.println(palindrom(user_input.next()));
+//     }
+        System.out.println(palindrom("aaa"));
+        System.out.println(palindrom("aaaaaa"));
+        System.out.println(palindrom("aviddiva"));
+        System.out.println(palindrom("avideedxiva"));
+        System.out.println(palindrom("avidxeexiva"));
+        System.out.println(palindrom("avixderdiva"));
+        System.out.println(palindrom("avixefddiva"));
+        System.out.println(palindrom("avixeeddiva"));
     }
 
     // first checkt the string is equal in reverse order then -1
-    // aa
-    // aa aa
-    // avid diva
+    // aaa
+    // aaaaa
+    // aviddiva
 
-    // avideedxiva
-    // avid efd xiva
+    // avideedxiva        avixd e rdiva
+    // avidefdxiva
 
     // avix efd diva
     // avix eed diva
     // avbiddiva
     public static int palindrom(String sampleText) {
         int index = -1;
-        int reverseIndex = sampleText.length() - 1;
-        for (int i = 0; i < sampleText.length(); i++) {
-            if (((i + 1) == reverseIndex) && (sampleText.charAt(i + 1) == sampleText.charAt(reverseIndex))) {
-                break;
-            } else if (((i + 1) == reverseIndex) && (sampleText.charAt(i + 1) != sampleText.charAt(reverseIndex)) &&
-                    (index > -1)) {
-                return -1;
+        for (int i = 0; i <= sampleText.length() / 2; i++) {
+            if ((sampleText.charAt(i) == sampleText.charAt(sampleText.length() - 1 - i))) {
+                continue;
             } else {
-                index = -1;
-                if (sampleText.charAt(i) != sampleText.charAt(reverseIndex)) {
-                    if (sampleText.charAt(i) == sampleText.charAt(--reverseIndex)) {
-                        index = reverseIndex + 1;
-                    }
+                if (index > -1) {
+                    return -1;
 
                 } else {
-                    index = -1;
+                    if (sampleText.charAt(i) == sampleText.charAt(sampleText.length() - 1 - i - 1)) {
+                        index = sampleText.length() - 1 - i;
+                        sampleText = sampleText.substring(0,sampleText.length()-i-1) + ""+ sampleText.substring
+                                (sampleText.length()-i,sampleText.length());
+                    } else if (sampleText.charAt(i + 1) == sampleText.charAt(sampleText.length() - 1 - i)) {
+                        index = i;
+                        sampleText = sampleText.substring(0,i) + ""+ sampleText.substring
+                                (i+1,sampleText.length());
+                    } else {
+                        return -1;
+                    }
                 }
             }
-            --reverseIndex;
         }
-
+        return index;
+    }
 
 /*        if (sampleText.charAt(0) == sampleText.charAt(sampleText.length()))
             if (new StringBuilder(sampleText).reverse().toString().equals(sampleText)) {
@@ -65,7 +76,6 @@ public class Demo {
                 }
             }
         return -1;*/
-        return index;
-    }
+
 
 }
